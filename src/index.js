a4a26d6d0d31c873adbba13b1b6b73b2048dec5c
Natalya Bakhtina <nbakhtina87@gmail.com>
2026-06-19 01:08:24 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import process from 'process'
 const gendiff = (filepath1, filepath2) => {
   const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8')
   const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8')
-  const parsedDate1 = parse(content1)
-  const parsedDate2 = parse(content2)
-  console.log('parsedDate1', parsedDate1)
-  console.log('parsedDate2', parsedDate2)
-  return content1 === content2
+  const parsedData1 = parse(content1)
+  const parsedData2 = parse(content2)
+  console.log('parsedData1', parsedData1)
+  console.log('parsedData2', parsedData2)
+  return parsedData1 === parsedData2
 }
 
 export default gendiff
