1b98699573692adde536f039a4685a1e021251fd
Natalya Bakhtina <nbakhtina87@gmail.com>
2026-07-02 00:40:13 +0300

    step 5, step 4 in the previous commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/NataBakh/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/NataBakh/frontend-project-46/actions)
+
+**Описание**
+
+Вычислитель отличий – программа, которая определяет разницу между двумя структурами данных. Это популярная задача, для которой существуют онлайн-сервисы вроде jsondiff; похожий механизм используется при выводе тестов и при отслеживании изменений в конфигурационных файлах.
+
+**Возможности утилиты:**
+* Поддержка разных входных форматов: YAML, JSON
+* Генерация отчёта в форматах plain text, stylish и JSON
+
+**Требования**
+* Git клиент
+* Node.js 18 или выше
+* Make
+
+**Установка**<br>
+**Клонировать проект**<br>
+`$ git clone`<br>
+**Установить пакет(может потребоваться sudo)**<br>
+`$ make setup`
+
+**Пример использования**
+```# формат plain
+gendiff --format plain path/to/file.yml another/path/file.json
+
+Property 'common.follow' was added with value: false
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group2' was removed
+
+# формат stylish
+gendiff filepath1.json filepath2.json
+
+{
+  + follow: false
+    setting1: Value 1
+  - setting2: 200
+  - setting3: true
+  + setting3: {
+        key: value
+    }
+}
+```
+[![asciicast](https://asciinema.org/a/dAg6IgzCYUKDkdkA.svg)](https://asciinema.org/a/dAg6IgzCYUKDkdkA)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import path from 'path'
 import parse from './parser.js'
 import fs from 'fs'
-import process from 'process'
+import _ from 'lodash'
 
 const gendiff = (filepath1, filepath2) => {
-  const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8')
-  const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8')
+  const content1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8')
+  const content2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8')
+  
   const parsedData1 = parse(content1)
   const parsedData2 = parse(content2)
-  console.log('parsedData1', parsedData1)
-  console.log('parsedData2', parsedData2)
-  return parsedData1 === parsedData2
+ 
+  const keys1 = Object.keys(parsedData1);
+  const keys2 = Object.keys(parsedData2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return JSON.stringify(value); // Или кастомная функция для красивого вывода объектов
+  }
+  return String(value)
+}
+
+  const result = sortedKeys.flatMap((key) => {
+    if (!_.has(parsedData2, key)) {
+      return `  - ${key}: ${stringify(parsedData1[key])}`
+    }
+    if (!_.has(parsedData1, key)) {
+      return `  + ${key}: ${stringify(parsedData2[key])}`
+    }
+    if (parsedData1[key] === parsedData2[key]) {
+      return `    ${key}: ${stringify(parsedData1[key])}`
+    }
+     return [
+      `  - ${key}: ${stringify(parsedData1[key])}`,
+      `  + ${key}: ${stringify(parsedData2[key])}`
+    ]
+  })
+
+  return `{\n${result.join('\n')}\n}`
 }
 
 export default gendiff
